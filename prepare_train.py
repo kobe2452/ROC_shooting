10514ebdc62dc559ff81fc2dc7acb3a2471dfa8b
kobe2452 <tongliu0314@gmail.com>
2015-09-13 18:26:13 -0400

    add two filters to keep English tweets in the same timezone

--- prepare_train.py
+++ prepare_train.py
@@ -31,43 +31,53 @@ def read_daily_json_write_train_label(daily_json, record0, MONTHS, train_json, b
         tweet = json.loads(line.decode('utf-8'))
         # tweet.keys() = [u'filtering', u'contributors', u'truncated', u'text', u'in_reply_to_status_id', u'id', u'favorite_count', u'datetime retrieved', u'source', u'quoted_status_id', u'retweeted', u'coordinates', u'timestamp_ms', u'quoted_status', u'entities', u'in_reply_to_screen_name', u'id_str', u'retweet_count', u'in_reply_to_user_id', u'favorited', u'user', u'geo', u'in_reply_to_user_id_str', u'possibly_sensitive', u'lang', u'created_at', u'quoted_status_id_str', u'filter_level', u'in_reply_to_status_id_str', u'place']
 
-        msg_id = tweet['id']
-        message = tweet['text']
+        language = tweet['lang']
 
-        user = tweet['user']       
-        # user.keys() = [u'follow_request_sent', u'profile_use_background_image', u'id', u'verified', u'profile_image_url_https', u'profile_sidebar_fill_color', u'is_translator', u'geo_enabled', u'profile_text_color', u'followers_count', u'protected', u'location', u'default_profile_image', u'id_str', u'utc_offset', u'statuses_count', u'description', u'friends_count', u'profile_link_color', u'profile_image_url', u'notifications', u'profile_background_image_url_https', u'profile_background_color', u'profile_banner_url', u'profile_background_image_url', u'screen_name', u'lang', u'profile_background_tile', u'favourites_count', u'name', u'url', u'created_at', u'contributors_enabled', u'time_zone', u'profile_sidebar_border_color', u'default_profile', u'following', u'listed_count']
-        userid = user['id']
+        # only study English tweets
+        if language == u'en':
 
-        timestamp_ms = float(tweet['timestamp_ms'])
+            msg_id = tweet['id']
+            message = tweet['text']
 
-        # method 1 using time.ctime
-        # local_output1 = timestamp2datetime(timestamp_ms)
+            user = tweet['user']     
+            # user.keys() = [u'follow_request_sent', u'profile_use_background_image', u'id', u'verified', u'profile_image_url_https', u'profile_sidebar_fill_color', u'is_translator', u'geo_enabled', u'profile_text_color', u'followers_count', u'protected', u'location', u'default_profile_image', u'id_str', u'utc_offset', u'statuses_count', u'description', u'friends_count', u'profile_link_color', u'profile_image_url', u'notifications', u'profile_background_image_url_https', u'profile_background_color', u'profile_banner_url', u'profile_background_image_url', u'screen_name', u'lang', u'profile_background_tile', u'favourites_count', u'name', u'url', u'created_at', u'contributors_enabled', u'time_zone', u'profile_sidebar_border_color', u'default_profile', u'following', u'listed_count']
+            userid = user['id']
 
-        # # method 2 provided by Chris
-        # created_at = tweet['created_at']
-        # utc_output = datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")
-        # local_output2 = utc_output - timedelta(hours = 4)
+            timestamp_ms = float(tweet['timestamp_ms'])
 
-        # method 3 using pytz, http://pytz.sourceforge.net/
-        local_output3 = pytz_function(timestamp_ms)[1]
+            # method 1 using time.ctime
+            # local_output1 = timestamp2datetime(timestamp_ms)
 
-        record = parse_datetime(local_output3)
+            # # method 2 provided by Chris
+            # created_at = tweet['created_at']
+            # utc_output = datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")
+            # local_output2 = utc_output - timedelta(hours = 4)
 
-        # if (record[0] <= record0[0]) and (MONTHS.index(record[1]) <= MONTHS.index(record0[1])) and (record[2] <= record0[2]) and (record[4] < record0[4]):
-        #     time_label = -1
-        #     before_dict[msg_id] += 1
-        # else:
-        #     time_label = +1
-        #     after_dict[msg_id] += 1
+            # method 3 using pytz, http://pytz.sourceforge.net/
+            local_output3 = pytz_function(timestamp_ms)[1]
 
-        # basic = {}
-        # basic['msg_id'] = msg_id
-        # basic['message'] = message
-        # basic['userid'] = userid
-        # basic['time_label'] = time_label
+            record = parse_datetime(local_output3)
 
-        # json.dump(basic, train_json)
-        # train_json.write("\n")
+            if record[7] == record0[7]:
+
+                if (record[0] <= record0[0]) and (MONTHS.index(record[1]) <= MONTHS.index(record0[1])) and (record[2] <= record0[2]) and (record[4] < record0[4]):
+
+                    time_label = -1
+                    before_dict[msg_id] += 1
+
+                else:
+
+                    time_label = +1
+                    after_dict[msg_id] += 1
+
+            basic = {}
+            basic['msg_id'] = msg_id
+            basic['message'] = message
+            basic['userid'] = userid
+            basic['time_label'] = time_label
+
+            json.dump(basic, train_json)
+            train_json.write("\n")
 
 def parse_datetime(datetime_format):
 
